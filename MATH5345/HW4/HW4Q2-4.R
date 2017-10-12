244f375355a414c8bf083de6cd019394a64da492
Joshua Lelon Mitchell <jlelonmitchell@gmail.com>
2017-10-12 15:13:17 -0500

    stats hw done

--- MATH5345/HW4/HW4Q2-4.R
+++ MATH5345/HW4/HW4Q2-4.R
@@ -1,3 +1,4 @@
+# MATH 5345 HW 4 Q2 - Q4 - Joshua Mitchell
 setwd("/Users/jm/joshualmitchell.github.io/MATH5345/HW4")
 data <- read.table("3.1data.txt", header=T)
 data
@@ -9,74 +10,70 @@ x = data[,c(2:last_column)]
 y = data[,1]
 
 ####################################################
-#Q1: a) ############################################
+#Q2: c) ############################################
 ####################################################
 
-model <- lm(y ~ x$x2 + x$x7 + x$x8, data=data)
+x2 <- x$x2
+x7 <- x$x7
+x8 <- x$x8
+
+model <- lm(y ~ x2 + x7 + x8, data=data)
+summary(model)
 # xtable(model) # Used to generate LaTeX versions
 
 ####################################################
-#Q1: b) ############################################
+#Q2: d) ############################################
 ####################################################
 
-anova(model)
-# xtable(anova(model))
-
-# Failing to reject H_0: B_1 = B_2 = ... B_k is equivalent to saying:
-# there is no linear relationship between y and any of the regressor variables.
-
-# Find test statistic:
-
-# SS_T = SS_R + SS_Res
-# SS_R = B_hat\pr X\pr y - (sumin y_i)^2 / n, SS_Res = y\pr y - B_hat\pr X\pr y
-# SS_R = B_hat\pr X\pr y - (sumin y_i)^2 / n
-# SS_Res = y\pr y - B_hat\pr X\pr y
-# SS_T = SS_R + SS_Res
-
-
-# Reject H0 if F_0 > F_afa, k, n - k - 1
-
-
-B_1_hat <- -7.404
-
-SS_T <- sum((y - mean(y))^2)
-S_xy <- sum((y * (x - mean(x))))
-S_xx <- sum((x - mean(x))^2)
-SS_res <- SS_T - B_1_hat * S_xy
-n <- 16
-MS_res <- SS_res / (n - 2)
-SE <- sqrt(MS_res / S_xx)
-t_0 <- B_1_hat / SE
-t_0 # -0.5994534
-
-# Reject if |t_0| > t_alpha/2, n-2
-
-# |-0.5994534| > 2.145 (t_0.05/2, 14)
-
-# Therefore, accept H_0
+# summary(model)$r.squared # yields 0.7863069
+# summary(model)$adj.r.squared # yields 0.7595953
 
 ####################################################
-#Q1: c) ############################################
+#Q2: f) ############################################
 ####################################################
 
-B_1_hat <- 11.80
+confint(model)
 
-SS_T <- sum((y - mean(y))^2)
-S_xy <- sum((y * (x - mean(x))))
-S_xx <- sum((x - mean(x))^2)
-S_xx # 1.064975
-SS_res <- SS_T - B_1_hat * S_xy
-n <- 20
-MS_res <- SS_res / (n - 2)
-MS_res #12.93596
-SE <- sqrt(MS_res / S_xx)
-t_0 <- B_1_hat / SE
-t_0 # 3.38573
+# x$x7: (0.011855322, 0.376065098)
 
-# We reject H_0 if |t_0| > t_alpha/2, n-2.
-# 3.38612 > t_alpha/2, n-2
-# 3.38612 > 2.101
-# Therefore, reject H_0
+####################################################
+#Q2: g) ############################################
+####################################################
+
+test_X <- data.frame(x2=2300, x7=56, x8=2100)
+predict(model, test_X, interval="confidence")
+
+####################################################
+#Q3: a) ############################################
+####################################################
+
+# Strip into y and x:
+model <- lm(y ~ x7 + x8, data=data)
+
+####################################################
+#Q3: b) ############################################
+####################################################
+
+summary(model)$r.squared
+summary(model)$adj.r.squared
+
+####################################################
+#Q3: c) ############################################
+####################################################
+
+confint(model)
+
+####################################################
+#Q3: d) ############################################
+####################################################
+
+test_X <- data.frame(x7=56, x8=2100)
+predict(model, test_X, interval="confidence")
+
+####################################################
+#Q3: e) ############################################
+####################################################
+
 
 ####################################################
 #Q4: a) ############################################
