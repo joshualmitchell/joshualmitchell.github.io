3ce24dc9a700d018f2e0b02cee978701bac07cc7
Joshua Lelon Mitchell <jlelonmitchell@gmail.com>
2017-11-09 13:22:41 -0600

    stats update

--- MATH5345/proj/proj.R
+++ MATH5345/proj/proj.R
@@ -10,13 +10,8 @@ colnames(autodata) <- c("mpg_c", "cylnum_mvd", "displ_c", "hp_c", "wgt_c", "acc_
 # -- to check the dimension, distribution, potential outliers for data (y, x1, x2...)
 # -- consider removing incorrect or "weird" data points (but be VERY careful)
 
-dim(autodata)
+str(autodata)
 summary(autodata)
-
-# autodata$hp_c <- gsub("?",NA, autodata$hp_c, fixed = TRUE)
-autodata <- subset(autodata, !autodata[ , 4] == "?")
-autodata$hp_c <- as.numeric(as.character(autodata$hp_c))
-
 
 # Check normality of "y" (we don't care about normality of x):
 hist(autodata$mpg_c, main="MPG Frequency", xlab="Miles per gallon", ylab="Number of cars with x miles per galon")
@@ -24,45 +19,11 @@ hist(autodata$mpg_c, main="MPG Frequency", xlab="Miles per gallon", ylab="Number
 # Make origin a factor (since it's not continuous)
 autodata$origin_mvd <- as.factor(autodata$origin_mvd)
 
-# Step 2: Fit a multiple linear regression model
-# -- Use lm to fit y ~ x1, x2 ..
-# -- Get estimated linear model, R2, ANOVA, test significance of regression
+# Remove data points with missing horsepower values and set them to numeric columns
+autodata <- subset(autodata, !autodata[ , 4] == "?")
+autodata$hp_c <- as.numeric(as.character(autodata$hp_c))
 
-full <- lm(mpg_c ~ cylnum_mvd + displ_c + hp_c + wgt_c + acc_c + modelyr_mvd + origin_mvd, data=autodata) 
-null <- lm(mpg_c ~ 1, data=autodata) 
-
-# Perform "forward" selection starting from the NULL model and set up the scope to have an upper = FULL model. 
-
-forw <- step(null, scope=list(lower=null, upper=full), direction="forward")
-forw.lm <- lm(mpg_c ~ wgt_c + modelyr_mvd + origin_mvd + displ_c + hp_c + cylnum_mvd, data = autodata)
-summary(forw.lm)
-anova(forw.lm)
-
-# Backwards Selection
-
-back <- step(full, data=autodata, direction="backward")
-back.lm <- lm(mpg_c ~ cylnum_mvd + displ_c + hp_c + wgt_c + modelyr_mvd + origin_mvd, data = autodata)
-summary(back.lm)
-anova(back.lm)
-
-# Stepwise Selection
-
-stepwise<- step(null, scope = list(upper=full), data=autodata, direction="both")
-stepwise.lm <- lm(mpg_c ~ wgt_c + modelyr_mvd + origin_mvd + displ_c + hp_c + cylnum_mvd, data = autodata)
-summary(stepwise.lm)
-anova(stepwise.lm)
-
-# All Possible Regressor Selection (using regsub)
-
-regsub.exhaust<-regsubsets(mpg_c ~ cylnum_mvd + displ_c + hp_c + wgt_c + acc_c + modelyr_mvd + origin_mvd, data=autodata, nbest = 1, nvmax = NULL,force.in = NULL, force.out = NULL, intercept=TRUE, method = "exhaustive")
-summary.out <- summary(regsub.exhaust)
-summary.out
-
-model_info <- cbind("# Regressors"=1:8, "R-squared"=summary.out$rsq, "adj R-squared"=summary.out$adjr2, "MS_res"=summary.out$rss/(nrow(autodata) - 2:9), "CP - p"=summary.out$cp - 2:9)
-
-# Let's check normality and constant variance:
-
-lm7 <- lm(mpg_c ~ wgt_c + modelyr_mvd + origin_mvd + hp_c + displ_c + cylnum_mvd + acc_c, data = autodata)
+# Let's check constant variance:
 
 # Residual Plots:
 
@@ -83,23 +44,30 @@ abline(h = 0, col="red")
 plot(autodata$acc_c, resid(lm7),  main="Residuals vs Acceleration", xlab="Acceleration", ylab="Residuals")
 abline(h = 0, col="red")
 
+lm7 <- lm(mpg_c ~ wgt_c + modelyr_mvd + origin_mvd + hp_c + displ_c + cylnum_mvd + acc_c, data = autodata)
+
 plot(lm7$fitted.values, resid(lm7), main="Residuals vs Fitted Values", xlab="Fitted Values", ylab="Residuals")
 abline(h = 0, col="red")
 
+# Much better! Let's check normality again:
+qqnorm(resid(lm7), main="Residuals vs Random Normal", xlab="Random Normal", ylab="Residuals")
+qqline(resid(lm7))
+
 # Oooh, has kind of a horseshoe pattern, let's apply a log transformation:
+
 lm7 <- lm(log(mpg_c) ~ wgt_c + modelyr_mvd + origin_mvd + hp_c + displ_c + cylnum_mvd + acc_c, data = autodata)
+
 plot(lm7$fitted.values, resid(lm7), main="[Transformed] Residuals vs Fitted Values", xlab="Residuals", ylab="Fitted Values")
 abline(h = 0, col="red")
 
 # Much better! Let's check normality again:
-qqnorm(resid(lm7))
+qqnorm(resid(lm7), main="[Transformed] Residuals vs Random Normal", xlab="Random Normal", ylab="Residuals")
 qqline(resid(lm7))
 # Still mostly normal, but now it has a lower left tail
 
-##################
-### STARTING OVER
-### ISH
-##################
+# Step 2: Fit a multiple linear regression model
+# -- Use lm to fit y ~ x1, x2 ..
+# -- Get estimated linear model, R2, ANOVA, test significance of regression
 
 full <- lm(log(mpg_c) ~ cylnum_mvd + displ_c + hp_c + wgt_c + acc_c + modelyr_mvd + origin_mvd, data=autodata) 
 null <- lm(log(mpg_c) ~ 1, data=autodata) 
