300b65c0d27f180b62fb712329951fc55e20e224
Joshua Lelon Mitchell <jlelonmitchell@gmail.com>
2017-11-16 21:07:37 -0600

    Rough draft of HW 6 Stats

--- MATH5345/HW6/HW6.R
+++ MATH5345/HW6/HW6.R
@@ -1,22 +1,24 @@
-install.packages("leaps")
-install.packages("car")
-install.packages("alr3")
+# install.packages("leaps")
+# install.packages("car")
+# install.packages("alr3")
 library(leaps)
 library(car)
 library(xtable)
+library(alr3)
+
 # ?regsubsets. # No documentation for ‘regsubsets.’ in specified packages and libraries
 setwd("/Users/jm/joshualmitchell.github.io/MATH5345/HW6/")
-
-library(alr3)
-pureErrorAnova(model)
 
 ########################################################################################
 ###################### Question 1
 ########################################################################################
 
+## A
+
 q1data <- read.table("HW6Q1.txt", header=T)
 
 q1lm <- lm(y ~ as.factor(x1) + as.factor(x2) + as.factor(x3), data=q1data)
+qqnorm(q1data$y)
 summary(q1lm)
 plot(q1data$x1, resid(q1lm),  main="Residuals vs x1", xlab="x1", ylab="Residuals")
 abline(h = 0, col="red")
@@ -27,6 +29,9 @@ abline(h = 0, col="red")
 plot(resid(q1lm), q1lm$fitted.values)
 # plot(model) --> graph 4 indicates which points are "causing problems", graph 3 "scale location" for non-constant variance (lots of variance "in the middle" in this case)
 
+## B
+
+pureErrorAnova(q1lm)
 
 ########################################################################################
 ###################### Question 2
@@ -69,18 +74,78 @@ abline(h = 0, col="red")
 ########################################################################################
 
 q3data <- read.table("HW6Q3.txt", header=T)
-q3lm <- lm(y ~ x1 + x2 + x3 + x4, data = q3data)
 
 ## A
 
+q3lm <- lm(y ~ x1 + x2 + x3 + x4, data = q3data)
 hist(q3data$y)
+qqnorm(q3data$y)
+qqline(q3data$y)
 plot(q3lm$fitted.values, resid(q3lm), main="Residuals vs Fitted", xlab="Fitted", ylab="Residuals")
+abline(h = 0, col="red")
+plot(q3data$x1, resid(q3lm), main="Residuals vs x1", xlab="x1", ylab="Residuals")
+abline(h = 0, col="red")
+plot(q3data$x2, resid(q3lm), main="Residuals vs x2", xlab="x2", ylab="Residuals")
+abline(h = 0, col="red")
+plot(q3data$x3, resid(q3lm), main="Residuals vs x3", xlab="x3", ylab="Residuals")
+abline(h = 0, col="red")
+plot(q3data$x4, resid(q3lm), main="Residuals vs x4", xlab="x4", ylab="Residuals")
 abline(h = 0, col="red")
 summary(q3lm)
 
+## C (this is just repeating A for C, B is below)
+
+q3lm_data_with_infl <- influence.measures(q3lm)
+q3lm_inflpnts <- which(apply(q3lm_data_with_infl$is.inf, 1, any)) 
+data_wo_inflpnts <- q3data[-q3lm_inflpnts,]
+
+q3lm_wo_infl <- lm(y ~ x1 + x2 + x3 + x4, data = data_wo_inflpnts)
+hist(data_wo_inflpnts$y)
+qqnorm(data_wo_inflpnts$y)
+qqline(data_wo_inflpnts$y)
+plot(q3lm_wo_infl$fitted.values, resid(q3lm_wo_infl), main="(sans-infl) Residuals vs Fitted", xlab="Fitted", ylab="Residuals")
+abline(h = 0, col="red")
+plot(data_wo_inflpnts$x4, resid(q3lm_wo_infl), main="(sans-infl) Residuals vs x4", xlab="x4", ylab="Residuals")
+abline(h = 0, col="red")
+summary(q3lm_wo_infl)
+anova(q3lm_wo_infl)
+
 ## B
 
-q3lm_fact <- lm(y ~ as.factor(x1) + as.factor(x2) + as.factor(x3) + x4, data = q3data)
+q3lm_log <- lm(log(y) ~ x1 + x2 + x3 + x4, data = q3data)
+
+hist(log(q3data$y))
+qqnorm(log(q3data$y))
+qqline(log(q3data$y))
+
+plot(q3lm_log$fitted.values, resid(q3lm_log), main="Residuals vs Fitted", xlab="Fitted", ylab="Residuals")
+abline(h = 0, col="red")
+plot(q3data$x1, resid(q3lm_log), main="Residuals vs x1", xlab="x1", ylab="Residuals")
+abline(h = 0, col="red")
+plot(q3data$x2, resid(q3lm_log), main="Residuals vs x2", xlab="x2", ylab="Residuals")
+abline(h = 0, col="red")
+plot(q3data$x3, resid(q3lm_log), main="Residuals vs x3", xlab="x3", ylab="Residuals")
+abline(h = 0, col="red")
+plot(q3data$x4, resid(q3lm_log), main="Residuals vs x4", xlab="x4", ylab="Residuals")
+abline(h = 0, col="red")
+summary(q3lm_log)
+
+## C
+q3lm_log_data_with_infl <- influence.measures(q3lm_log)
+q3lm_log_inflpnts <- which(apply(q3lm_log_data_with_infl$is.inf, 1, any)) 
+log_data_wo_inflpnts <- q3data[-q3lm_log_inflpnts,]
+
+q3lm_log_wo_infl <- lm(log(y) ~ x1 + x2 + x3 + x4, data = log_data_wo_inflpnts)
+
+hist(log(log_data_wo_inflpnts$y))
+qqnorm(log(log_data_wo_inflpnts$y))
+qqline(log(log_data_wo_inflpnts$y))
+plot(q3lm_log_wo_infl$fitted.values, resid(q3lm_log_wo_infl), main="(sans-infl) (log) Residuals vs Fitted", xlab="Fitted", ylab="Residuals")
+abline(h = 0, col="red")
+plot(log_data_wo_inflpnts$x4, resid(q3lm_log_wo_infl), main="(sans-infl) (log) Residuals vs x4", xlab="x4", ylab="Residuals")
+abline(h = 0, col="red")
+summary(q3lm_log_wo_infl)
+anova(q3lm_log_wo_infl)
 
 ########################################################################################
 ###################### Question 4
