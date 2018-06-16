c41e0dc87081a371dafda4a280302cd933437095
Joshua Lelon Mitchell <jlelonmitchell@gmail.com>
2018-06-16 13:14:32 -0500

    deleted more stuff

--- p.sh
+++ p.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-read -p "Commit description: " desc
-git add . && \
-git add -u && \
-git commit -m "$desc" && \
-git push origin master
+#!/bin/sh
+git add --all
+git commit -m "$1"
+git push
+echo "Committed and pushed with this commit message: $1"
